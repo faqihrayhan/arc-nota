fc575552b4eb25d1c03d2475698e5b71ee6475e2
faqihrayhan <fqhrayhan19@gmail.com>
2026-08-04 02:03:04 +0700

    Fix : Payment and allv1

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,9 +5,16 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
+  {
+    rules: {
+      "react-hooks/set-state-in-effect": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@next/next/no-html-link-for-pages": "off"
+    }
+  },
+  // Override default ignores of eslint-config-next
   globalIgnores([
-    // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
@@ -16,5 +23,3 @@ const eslintConfig = defineConfig([
 ]);
 
 export default eslintConfig;
-
-
